4834d40027a273ec0e77375f7e69e83c6a9ffdcf
Gustavo Lopes <132415343+o-gustavolopes@users.noreply.github.com>
2026-05-20 00:08:21 -0300

    update 2

--- src/main/java/com/padrao/visitor/RelatorioVisitor.java
+++ src/main/java/com/padrao/visitor/RelatorioVisitor.java
@@ -1,21 +1,23 @@
 package com.padrao.visitor;
 
+import java.util.Locale;
+
 public class RelatorioVisitor implements CardapioVisitor {
 
     @Override
     public String visitarBebida(Bebida bebida) {
         String tipo = bebida.isAlcolica() ? "Alcoólica" : "Não alcoólica";
-        return String.format("[BEBIDA]    %-25s R$ %6.2f  (%s)", bebida.getNome(), bebida.getPreco(), tipo);
+        return String.format(Locale.US, "[BEBIDA]    %-25s R$ %6.2f  (%s)", bebida.getNome(), bebida.getPreco(), tipo);
     }
 
     @Override
     public String visitarPrato(Prato prato) {
-        return String.format("[PRATO]     %-25s R$ %6.2f  Categoria: %s", prato.getNome(), prato.getPreco(), prato.getCategoria());
+        return String.format(Locale.US, "[PRATO]     %-25s R$ %6.2f  Categoria: %s", prato.getNome(), prato.getPreco(), prato.getCategoria());
     }
 
     @Override
     public String visitarSobremesa(Sobremesa sobremesa) {
         String gluten = sobremesa.isSemGluten() ? "Sem glúten" : "Com glúten";
-        return String.format("[SOBREMESA] %-25s R$ %6.2f  (%s)", sobremesa.getNome(), sobremesa.getPreco(), gluten);
+        return String.format(Locale.US, "[SOBREMESA] %-25s R$ %6.2f  (%s)", sobremesa.getNome(), sobremesa.getPreco(), gluten);
     }
 }
